10d939ddba30da58d96705994ff93cd0758b886a
Tucker Hawkinson <7201606-tucker.hawkinson@users.noreply.gitlab.com>
2020-12-16 16:47:54 -0500

    updated index

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 const DataMonger = require('data-monger');
 const pg = require('pg');
+const mysql = require('mysql');
+const mongodb = require('mongodb');
 
-console.log(new DataMonger(pg));
+let dataMonger = new DataMonger({
+  dbType: 'postgres',
+  driver: pg,
+  connectionInfo: {
+    host: 'localhost',
+    port: 5432,
+    user: 'postgres',
+    password: 'password',
+    database: 'DataMonger',
+  },
+});
