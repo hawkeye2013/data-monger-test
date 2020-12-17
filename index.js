c285aa44500bdb45f7e70ae21bc41f3608a2023f
Tucker Hawkinson <7201606-tucker.hawkinson@users.noreply.gitlab.com>
2020-12-16 19:37:32 -0500

    updating to test get portins

--- index.js
+++ index.js
@@ -2,15 +2,35 @@ const DataMonger = require('data-monger');
 const pg = require('pg');
 const mysql = require('mysql');
 const mongodb = require('mongodb');
+const sqlite = require('sqlite3');
 
-let dataMonger = new DataMonger({
-  dbType: 'postgres',
-  driver: pg,
-  connectionInfo: {
-    host: 'localhost',
-    port: 5432,
-    user: 'postgres',
-    password: 'password',
-    database: 'DataMonger',
+let db = new DataMonger({
+  dbType: 'SQLite',
+  driver: sqlite,
+  driverConstructor: 'data.db',
+  options: {
+    verbose: true,
   },
 });
+
+db.collection('employees')
+  .findOne({
+    EmployeeId: 1,
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+db.collection('employees')
+  .find({
+    Title: 'Sales Support Agent',
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
